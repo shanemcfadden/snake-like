2c734bd82ccc192b06ef46d64c163ed99997444c
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2025-08-31 14:34:29 -0500

    Add custom lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,9 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      "arrow-body-style": ["error", "as-needed"],
+      curly: ["error", "all"],
+    },
   },
 ]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-const App = () => {
-  return <h1>Hello Vite!</h1>;
-};
+const App = () => <h1>Hello Vite!</h1>;
 
 export default App;
