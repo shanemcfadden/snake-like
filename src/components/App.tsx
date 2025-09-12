098afe151d1068b8392203dd4baf36a8f63c4409
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2025-09-11 20:40:46 -0500

    Format after linting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,12 +4,12 @@ import { Controller } from "./Controller";
 import { Scoreboard } from "./Scoreboard";
 
 export const App = () => (
-    <Container>
-      <h1 className="my-4 text-center text-xl text-primary">Snake-like</h1>
-      <Scoreboard />
-      <Display />
-      <div className="flex justify-center my-4">
-        <Controller />
-      </div>
-    </Container>
-  );
+  <Container>
+    <h1 className="my-4 text-center text-xl text-primary">Snake-like</h1>
+    <Scoreboard />
+    <Display />
+    <div className="flex justify-center my-4">
+      <Controller />
+    </div>
+  </Container>
+);
