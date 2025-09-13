e63c607017794b4173b6404763fc949f1348cdad
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2025-09-12 22:13:26 -0500

    Make cursor pointer on game button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,18 @@ type ButtonProps = PropsWithChildren<{
 
 export const Button = ({ disabled, onClick, children }: ButtonProps) => (
   <button
-    className="bg-primary text-background border-black px-4 py-2 hover:bg-secondary disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:bg-primary"
+    className={[
+      "bg-primary",
+      "border-black",
+      "cursor-pointer",
+      "disabled:cursor-not-allowed",
+      "disabled:hover:bg-primary",
+      "disabled:opacity-50",
+      "hover:bg-secondary",
+      "px-4",
+      "py-2",
+      "text-background",
+    ].join(" ")}
     disabled={disabled}
     onClick={onClick}
   >
