9c5263f9b468d70fc3fa3093a84433191f043500
Shane McFadden <shane.t.mcfadden@gmail.com>
2025-09-20 22:13:41 -0500

    Add link to GitHub source (#13)

---
+++ src/components/Banner.tsx
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareGithub } from "@fortawesome/free-brands-svg-icons";
+import { memo } from "react";
+
+export const Banner = memo(() => (
+  <div className="flex justify-between">
+    <div className="w-12" />
+    <h1 className="my-4 text-center text-xl">Snake-like</h1>
+    <div className="w-12 text-right my-auto text-3xl">
+      <a href={GITHUB_REPOSITORY_URL} target="_blank" rel="noreferrer">
+        <FontAwesomeIcon
+          icon={faSquareGithub}
+          title="View source on GitHub"
+          widthAuto
+        />
+      </a>
+    </div>
+  </div>
+));
+
+const GITHUB_REPOSITORY_URL = "https://github.com/shanemcfadden/snake-like";
